dfde1993ae88005a4a4de63714f8e8817e4b2532
AjinkyaGhugarkar <94744493+AjinkyaGhugarkar@users.noreply.github.com>
2022-03-12 08:35:24 +0530

    Add files via upload

---
+++ arrayProgram/ThreeDiamensionArray.java
@@ -0,0 +1,10 @@
+package arraysProgram;
+
+public class ThreeDiamensionArray {
+
+	public static void main(String[] args) {
+		 //2 2 2
+		int arr[][][] = {{{1,2,3},{11,12}},{{21,22,23,24},{31,32},{51,52}}};
+		System.out.println(arr[1][0][1]);
+		}
+}
---
+++ arrayProgram/TwoDimesionalArray.java
@@ -0,0 +1,28 @@
+package arraysProgram;
+
+public class TwoDimesionalArray {
+
+	public static void main(String[] args) {
+		int arr[][]=new int[3][2];   //two diamensional array declaration
+		arr[0][0]=10;
+		arr[0][1]=20;
+		arr[1][0]=30;
+		arr[1][1]=40;
+	    arr[2][0]=50;
+	    arr[2][1]=60;
+	System.out.println(arr[0][0]);	   
+    System.out.println(arr[0][1]);     
+    System.out.println(arr[1][0]);
+    System.out.println(arr[1][1]);
+    System.out.println(arr[2][0]);
+    System.out.println(arr[2][1]);
+    
+    
+    int num[]  = {1,2,3,4,5,69};
+   
+     System.out.println(num[5]);
+   
+    
+	}
+
+}
